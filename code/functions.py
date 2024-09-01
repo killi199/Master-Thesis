67378fa2931a96b56a460b2a84d3f0b56d095846
st230580 <k.jahrens@stud.hs-wismar.de>
2024-09-01 14:42:34 +0200

    Create function for python authors and maintainers

--- code/functions.py
+++ code/functions.py
@@ -214,6 +214,36 @@ async def get_pypi_maintainers(package_name: str) -> pd.DataFrame:
 
     return pd.DataFrame(pypi_maintainers_names, columns=['login', 'name'])
 
+def get_python_authors(pypi_data) -> pd.DataFrame:
+    python_author_email = pypi_data["info"]["author_email"]
+    if python_author_email == '' or python_author_email == None:
+        python_author_emails = []
+    else:
+        python_author_emails = python_author_email.split(", ")
+
+    python_author = pypi_data["info"]["author"]
+    if python_author == '' or python_author == None:
+        python_authors = []
+    else:
+        python_authors = python_author.split(", ")
+
+    return combine_name_email(python_authors, python_author_emails)
+
+def get_python_maintainers(pypi_data) -> pd.DataFrame:
+    python_maintainer_email = pypi_data["info"]["maintainer_email"]
+    if python_maintainer_email == '' or python_maintainer_email == None:
+        python_maintainer_emails = []
+    else:
+        python_maintainer_emails = python_maintainer_email.split(", ")
+
+    python_maintainer = pypi_data["info"]["maintainer"]
+    if python_maintainer == '' or python_maintainer == None:
+        python_maintainers = []
+    else:
+        python_maintainers = python_maintainer.split(", ")
+
+    return combine_name_email(python_maintainers, python_maintainer_emails)
+
 def combine_name_email(names: list[str], emails: list[str]) -> pd.DataFrame:
     dic = []
 
