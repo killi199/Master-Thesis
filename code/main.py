d03fbb0ed1d91db2d672ce1cbcaa5865bd2b1657
st230580 <k.jahrens@stud.hs-wismar.de>
2024-09-02 12:59:50 +0200

    Refactor main

--- code/main.py
+++ code/main.py
@@ -5,7 +5,7 @@ import os
 import json
 from tqdm import tqdm
 
-async def process_package(package):
+async def process_pypi_package(package):
     package_name = package.strip()
     try:
         async with aiohttp.ClientSession() as session:
@@ -14,41 +14,40 @@ async def process_package(package):
 
         owner, repo, repo_link = functions.get_pypi_repo(pypi_data)
 
-        os.mkdir(f'results/{package_name}')
+        os.mkdir(f'results/pypi/{package_name}')
 
         git_contributors_df = await functions.get_git_contributors(owner, repo, repo_link, package_name)
-        git_contributors_df.to_csv(f'results/{package_name}/git_contributors.csv', index=False)
+        git_contributors_df.to_csv(f'results/pypi/{package_name}/git_contributors.csv', index=False)
+
+        async def process_and_save(dataframe, filename):
+            result = functions.matching(dataframe, git_contributors_df)
+            if not result.empty:
+                result.to_csv(f'results/pypi/{package_name}/{filename}.csv', index=False)
 
         # currently not fully accessible through the API -> limiting factor HTTP is limited if this is not used more semaphores possible
         pypi_maintainers_df = await functions.get_pypi_maintainers(package_name)
-        result = functions.matching(pypi_maintainers_df, git_contributors_df)
-        if not result.empty: result.to_csv(f'results/{package_name}/pypi_maintainers.csv', index=False)
+        await process_and_save(pypi_maintainers_df, 'pypi_maintainers')
 
         python_authors_df = functions.get_python_authors(pypi_data)
-        result = functions.matching(python_authors_df, git_contributors_df)
-        if not result.empty: result.to_csv(f'results/{package_name}/python_authors.csv', index=False)
+        await process_and_save(python_authors_df, 'python_authors')
 
         python_maintainers_df = functions.get_python_maintainers(pypi_data)
-        result = functions.matching(python_maintainers_df, git_contributors_df)
-        if not result.empty: result.to_csv(f'results/{package_name}/python_maintainers.csv', index=False)
+        await process_and_save(python_maintainers_df, 'python_maintainers')
 
         cff_df = functions.get_cff_authors(owner, repo)
-        result = functions.matching(cff_df, git_contributors_df)
-        if not result.empty: result.to_csv(f'results/{package_name}/cff_authors.csv', index=False)
+        await process_and_save(cff_df, 'cff_authors')
 
-        cff_df = functions.get_cff_preferred_citation_authors(owner, repo)
-        result = functions.matching(cff_df, git_contributors_df)
-        if not result.empty: result.to_csv(f'results/{package_name}/cff_preferred_citation_authors.csv', index=False)
+        cff_preferred_df = functions.get_cff_preferred_citation_authors(owner, repo)
+        await process_and_save(cff_preferred_df, 'cff_preferred_citation_authors')
 
         bib_df = functions.get_bib_authors(owner, repo)
-        result = functions.matching(bib_df, git_contributors_df)
-        if not result.empty: result.to_csv(f'results/{package_name}/bib_authors.csv', index=False)
+        await process_and_save(bib_df, 'bib_authors')
     except Exception as e:
         print(f"Error processing {package_name}: {e}")
 
-async def process_package_semaphore(package_name: str, semaphore: asyncio.Semaphore):
+async def process_package_semaphore(package_name: str, semaphore: asyncio.Semaphore, function):
     async with semaphore:
-        await process_package(package_name)
+        await function(package_name)
 
 async def main():
     # https://hugovk.github.io/top-pypi-packages/
@@ -59,7 +58,7 @@ async def main():
 
     semaphore = asyncio.Semaphore(2)
 
-    tasks = [process_package_semaphore(package['project'], semaphore) for package in rows]
+    tasks = [process_package_semaphore(package['project'], semaphore, process_pypi_package) for package in rows]
 
     for task in tqdm(asyncio.as_completed(tasks), total=len(tasks)):
         await task
