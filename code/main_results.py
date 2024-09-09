136c396a27f291acdfe156ccbc0a5a3307567cad
st230580 <k.jahrens@stud.hs-wismar.de>
2024-09-09 12:22:01 +0200

    Add non matches as result

--- code/main_results.py
+++ code/main_results.py
@@ -5,13 +5,8 @@ def check_matches(file_path):
     df = pd.read_csv(file_path)
     matches = (df['score'] > 0).sum()
     non_matches = (df['score'] == 0).sum()
-    return matches, non_matches
-
-def calculate_percentage(matches, non_matches):
-    total = matches + non_matches
-    if total == 0:
-        return 0
-    return (matches / total) * 100
+    entries = len(df)
+    return matches, non_matches, entries
 
 def process_directory(directory):
     file_type_percentages = {}
@@ -21,15 +16,16 @@ def process_directory(directory):
             if file == 'git_contributors.csv':
                 continue
             file_path = os.path.join(root, file)
-            matches, non_matches = check_matches(file_path)
+            matches, non_matches, entries = check_matches(file_path)
 
             file = os.path.splitext(file)[0]
             if file not in file_type_percentages:
-                file_type_percentages[file] = {'matches': 0, 'non_matches': 0}
+                file_type_percentages[file] = {'matches': 0, 'non_matches': 0, 'entries': 0}
             file_type_percentages[file]['matches'] += matches
             file_type_percentages[file]['non_matches'] += non_matches
+            file_type_percentages[file]['entries'] += entries
 
-    file_type_percentages = {ft: (data['matches'], data['non_matches'] + data['matches']) for ft, data in file_type_percentages.items()}
+    file_type_percentages = {ft: (data['matches'], data['non_matches'], data['entries']) for ft, data in file_type_percentages.items()}
 
     return file_type_percentages
 
@@ -40,11 +36,11 @@ def main():
     cran_file_types = process_directory(cran_dir)
     pypi_file_types = process_directory(pypi_dir)
 
-    for file_type, percentage in cran_file_types.items():
-        print(f"CRAN {file_type} {percentage[0]}/{percentage[1]}")
+    for file_type, matches in cran_file_types.items():
+        print(f"CRAN {file_type} {matches[0]}/{matches[2]} non matches {matches[1]}")
 
-    for file_type, percentage in pypi_file_types.items():
-        print(f"PyPi {file_type} {percentage[0]}/{percentage[1]}")
+    for file_type, matches in pypi_file_types.items():
+        print(f"PyPi {file_type} {matches[0]}/{matches[2]} non matches {matches[1]}")
 
 if __name__ == "__main__":
     main()
