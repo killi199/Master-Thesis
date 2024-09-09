6700771765eaaa9d03fb3217ad075d6428ec3140
st230580 <k.jahrens@stud.hs-wismar.de>
2024-09-09 12:42:03 +0200

    Add comparison of email and name

--- code/functions.py
+++ code/functions.py
@@ -51,6 +51,10 @@ def matching(df: pd.DataFrame, git_contributors_df: pd.DataFrame) -> pd.DataFram
                 contributor_email_edited = contributor['email'].split('@')[0].lower()
                 if author["login"].lower() in contributor_email_edited or contributor_email_edited in author["login"].lower() or fuzz.ratio(author["login"].lower(), contributor_email_edited) >= 80:
                     counter += 1
+            if counter == 0 and 'name' in df.columns and 'email' in df.columns and author["name"] is not None and contributor["email"] is not None:
+                contributor_email_edited = contributor['email'].split('@')[0].lower()
+                if author["name"].lower() in contributor_email_edited or contributor_email_edited in author["name"].lower() or fuzz.ratio(author["name"].lower(), contributor_email_edited) >= 80:
+                    counter += 1
 
             if counter > 0:
                 dic.append({
