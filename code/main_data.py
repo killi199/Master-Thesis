718efa3dc2395e53d58f12b4c4b1fdb6ff94c732
st230580 <k.jahrens@stud.hs-wismar.de>
2024-10-18 10:23:02 +0200

    Add semaphore for api

--- code/main_data.py
+++ code/main_data.py
@@ -41,7 +41,7 @@ async def process_general_package(owner: str, repo: str, git_contributors_df, pa
             await process_and_save(result, package_name, readme_author_df[1].strftime("%Y%m%d_%H%M%S") + '_readme_authors', index)
     await process_and_save(readme_df, package_name, 'readme', index)
 
-async def process_pypi_package(package):
+async def process_pypi_package(package, semaphore: asyncio.Semaphore):
     package_name = package.strip()
     try:
         async with aiohttp.ClientSession() as session:
@@ -56,9 +56,10 @@ async def process_pypi_package(package):
         git_contributors_df.to_csv(f'results/pypi/{package_name}/git_contributors.csv', index=False)
 
         # currently not fully accessible through the API -> limiting factor HTTP is limited if this is not used more semaphores possible
-        pypi_maintainers_df = await functions.get_pypi_maintainers(package_name)
-        result = functions.matching(pypi_maintainers_df, git_contributors_df)
-        await process_and_save(result, package_name, 'pypi_maintainers', 'pypi')
+        async with semaphore:
+            pypi_maintainers_df = await functions.get_pypi_maintainers(package_name)
+            result = functions.matching(pypi_maintainers_df, git_contributors_df)
+            await process_and_save(result, package_name, 'pypi_maintainers', 'pypi')
 
         python_authors_df = functions.get_python_authors(pypi_data)
         result = functions.matching(python_authors_df, git_contributors_df)
@@ -77,7 +78,7 @@ async def process_pypi_package(package):
     except Exception as e:
         print(f"Error processing {package_name}: {e}")
 
-async def process_cran_package(package):
+async def process_cran_package(package, _: asyncio.Semaphore):
     package_name = package.strip()
     try:
         async with aiohttp.ClientSession() as session:
@@ -108,9 +109,9 @@ async def process_cran_package(package):
     except Exception as e:
         print(f"Error processing {package_name}: {e}")
 
-async def process_package_semaphore(package_name: str, semaphore: asyncio.Semaphore, function):
+async def process_package_semaphore(package_name: str, semaphore: asyncio.Semaphore, function, pypi_api_semaphore):
     async with semaphore:
-        await function(package_name)
+        await function(package_name, pypi_api_semaphore)
 
 async def main():
     # https://hugovk.github.io/top-pypi-packages/
@@ -125,11 +126,11 @@ async def main():
         json_object = json.loads(packages[0])
         cran_rows = json_object['downloads'][:100]
 
-    pypi_semaphore = asyncio.Semaphore(2)
-    cran_semaphore = asyncio.Semaphore(10)
+    semaphore = asyncio.Semaphore(10)
+    pypi_api_semaphore = asyncio.Semaphore(1)
 
-    pypi_tasks = [process_package_semaphore(package['project'], pypi_semaphore, process_pypi_package) for package in pypi_rows]
-    cran_tasks = [process_package_semaphore(package['package'], cran_semaphore, process_cran_package) for package in cran_rows]
+    pypi_tasks = [process_package_semaphore(package['project'], semaphore, process_pypi_package, pypi_api_semaphore) for package in pypi_rows]
+    cran_tasks = [process_package_semaphore(package['package'], semaphore, process_cran_package, pypi_api_semaphore) for package in cran_rows]
 
     for task in tqdm(asyncio.as_completed(pypi_tasks), total=len(pypi_tasks)):
         await task
