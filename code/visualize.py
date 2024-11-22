d970744660d617ca8ae08e9c347749d16114d46b
st230580 <k.jahrens@stud.hs-wismar.de>
2024-11-22 20:55:34 +0100

    Add common authors visualisation

--- code/visualize.py
+++ code/visualize.py
@@ -6,6 +6,55 @@ import pandas as pd
 
 sources = {"PyPI": "pypi", "CRAN": "cran", "CFF": "cff", "PyPI CFF": "pypi_cff", "CRAN CFF": "cran_cff"}
 
+file_list = {"CFF": {"file": "common_authors_cff_authors_new.csv", "color": "blue"},
+             "CFF preferred citation": {"file": "common_authors_cff_preferred_citation_authors_new.csv", "color": "green"},
+             "CRAN Authors": {"file": "common_authors_cran_authors.csv", "color": "red"},
+             "CRAN Maintainers": {"file": "common_authors_cran_maintainers.csv", "color": "orange"},
+             "Beschreibung": {"file": "common_authors_description_authors.csv", "color": "purple"},
+             "Readme": {"file": "common_authors_readme_authors_new.csv", "color": "brown"},
+             "BibTeX": {"file": "common_authors_bib_authors_new.csv", "color": "cyan"},
+             "PyPI Maintainers": {"file": "common_authors_pypi_maintainers.csv", "color": "olive"},
+             "Python Authors": {"file": "common_authors_python_authors.csv", "color": "red"},
+             "Python Maintainers": {"file": "common_authors_python_maintainers.csv", "color": "orange"}}
+
+for source_name, source in sources.items():
+    for file_name, info in file_list.items():
+        file_path = f"overall_results/{source}/{info['file']}"
+        if not os.path.exists(file_path):
+            continue
+        df = pd.read_csv(file_path)
+        common_authors = []
+        total_authors = []
+        relative = [1]
+
+        for name in df.columns:
+            common_authors_package = ast.literal_eval(df[name][0])
+            total_authors_package = ast.literal_eval(df[name][1])
+            if len(common_authors) == 0:
+                common_authors = [0] * len(common_authors_package)
+                total_authors = [0] * len(total_authors_package)
+            relative_package = [1]
+
+            for index, common_author in enumerate(common_authors_package):
+                relative_package.append(common_author/total_authors_package[index])
+                common_authors[index] += common_authors_package[index]
+                total_authors[index] += total_authors_package[index]
+
+            plt.plot(relative_package, alpha=0.05)
+
+
+        for index, common_author in enumerate(common_authors):
+            relative.append(common_author/total_authors[index])
+
+        plt.plot(relative, label=file_name, color=info["color"])
+
+    plt.xlim(1, 100)
+    plt.legend(loc="upper right")
+    plt.title(f"Verhältnis der Top Git Autoren in der Zitation (Quelle: {source_name})")
+    plt.xlabel("Anzahl der betrachteten Git Autoren")
+    plt.ylabel("Verhältnis der genannten Autoren an den Git Autoren")
+    plt.show()
+
 file_list = {"CFF": {"file": "common_authors_2_cff_authors_new.csv", "color": "blue"},
              "CFF preferred citation": {"file": "common_authors_2_cff_preferred_citation_authors_new.csv", "color": "green"},
              "CRAN Authors": {"file": "common_authors_2_cran_authors.csv", "color": "red"},
@@ -39,7 +88,7 @@ for source_name, source in sources.items():
                 relative_package.append(common_author/total_authors_package)
                 common_authors[index] += common_author
 
-            plt.plot(relative_package, alpha=0.1)
+            plt.plot(relative_package, alpha=0.05)
 
 
         for common_author in common_authors:
@@ -48,9 +97,9 @@ for source_name, source in sources.items():
         plt.plot(relative, label=file_name, color=info["color"])
 
     plt.legend(loc="lower right")
-    plt.title(f"Anteil der {source_name} Autoren an den Git Autoren")
+    plt.title(f"Verhältnis der angegebenen Autoren am Quellcode (Quelle: {source_name})")
     plt.xlabel("Anzahl der betrachteten Git Autoren")
-    plt.ylabel("Anteil der gemeinsamen Autoren")
+    plt.ylabel("Verhältnis der genannten Autoren an den Git Autoren")
     plt.show()
 
 file_list = {"CFF": {"file": "total_authors_no_commits_cff_authors_new.csv", "color": "blue"},
@@ -90,7 +139,7 @@ for source_name, source in sources.items():
                 relative_package.append(authors/total_authors_package)
                 total_authors_no_commits[index] += authors
 
-            plt.plot(relative_package, alpha=0.1)
+            plt.plot(relative_package, alpha=0.05)
 
         if total_authors == 0:
             continue
@@ -102,7 +151,7 @@ for source_name, source in sources.items():
 
     plt.gca().invert_xaxis()
     plt.legend(loc="upper left")
-    plt.title(f"{source_name} Autoren ohne Commits")
+    plt.title(f"Autoren ohne Commits (Quelle: {source_name})")
     plt.xlabel("Tage")
-    plt.ylabel("Anteil der Autoren ohne Commits")
+    plt.ylabel("Verhältnis der Autoren ohne Commits")
     plt.show()
